b8c839eebb0b521ef3e5d8c42232636f68763e91
Mikail Asada <62200797+Asadaaaaa@users.noreply.github.com>
2023-03-16 09:58:42 +0700

    Add files via upload

--- js/Portfolio.js
+++ js/Portfolio.js
@@ -1,5 +1,17 @@
 let data = [
   {
+    title: "Fast-An",
+    desc: "College Assignment 'Pemrograman Web'",
+    img: "./assets/Fast-An.png",
+    redirect: "./porto/Fast-An/index.html"
+  },
+  {
+    title: "Learn HTML Table",
+    desc: "College Assignment 'Pemrograman Web'",
+    img: "./assets/RPL.png",
+    redirect: "./porto/Learn-HTML-Table/RPL-Main.html"
+  },
+  {
     title: "uSayit",
     desc: "Application that give netizens the freedom of speech to convey through many social media platforms, the message is anonymous, and can be seen by the general public.",
     img: "https://github.com/Asadaaaaa/uSayit/raw/main/uSayit.png",
